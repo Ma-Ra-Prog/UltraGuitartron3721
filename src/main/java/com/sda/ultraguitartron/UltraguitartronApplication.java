94e0bd31bdd197de3241c0a434f7e8380129dc5f
Ma-Ra-Prog <60273830+Ma-Ra-Prog@users.noreply.github.com>
2021-01-07 21:45:11 +0100

    Initial Commit

---
+++ src/main/java/com/sda/ultraguitartron/UltraguitartronApplication.java
@@ -0,0 +1,13 @@
+package com.sda.ultraguitartron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraguitartronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltraguitartronApplication.class, args);
+    }
+
+}
